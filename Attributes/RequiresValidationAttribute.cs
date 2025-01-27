b1d224fc5cc4e368f9b794df0e09b3cd689e5993
chronischeFliegenklatsche <nanuyalrif@gmail.com>
2025-01-27 14:00:49 +0100

    implement RequiresValidation attribute

---
+++ Attributes/RequiresValidationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookabookWPF.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class RequiresValidationAttribute : Attribute
+    {
+
+        // Delegate for the validation method
+        public delegate bool ValidationDelegate(object value);
+
+        public ValidationDelegate ValidationMethod { get; set; }
+
+
+        // Constructor
+        public RequiresValidationAttribute(ValidationDelegate validationMethod)
+        {
+            ValidationMethod = validationMethod;
+        }
+    }
+}
